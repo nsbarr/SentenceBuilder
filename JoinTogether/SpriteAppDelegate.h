8833226437a62971072187ba9999fbf56be48773
nsbarrcanvas <nick@canv.as>
2013-09-30 16:09:55 -0400

    Initial Commit

---
+++ JoinTogether/SpriteAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SpriteAppDelegate.h
+//  JoinTogether
+//
+//  Created by Nicholas Barr on 9/30/13.
+//  Copyright (c) 2013 Nicholas Barr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpriteAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JoinTogetherTests/JoinTogetherTests.m
@@ -0,0 +1,34 @@
+//
+//  JoinTogetherTests.m
+//  JoinTogetherTests
+//
+//  Created by Nicholas Barr on 9/30/13.
+//  Copyright (c) 2013 Nicholas Barr. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JoinTogetherTests : XCTestCase
+
+@end
+
+@implementation JoinTogetherTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
